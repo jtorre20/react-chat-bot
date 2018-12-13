ffc8a2b0c17e4203ddd7f0ec1a55fbe761bd7ee4
Julio <jtorres1989@gmail.com>
2018-12-13 17:56:26 -0500

    added background image, changed color

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { ChatBot } from 'aws-amplify-react';
 import { Interactions } from 'aws-amplify';
 import { ChatFeed, Message } from 'react-chat-ui'
-import logo from './logo.svg';
 import './App.css';
+import ironMan from './images/iron-man.jpg'
 
 class App extends Component {
 
@@ -13,7 +13,7 @@ class App extends Component {
       messages: [
         new Message({
           id: 1, 
-          message: "Hello, how can I help you today?",
+          message: "Boss, how can I help you today?",
         })
       ],
   }
@@ -73,21 +73,76 @@ class App extends Component {
   }
 
 
+
   render() {
+
+  const styles = {
+    bubbleStyles: {
+      text: {
+        fontSize: 16,
+      },
+      chatbubble: {
+        borderRadius: 30,
+        padding: 10,
+        backgroundColor: 'rgb(193,32,32)'
+      }
+    },
+    headerTitle: { 
+      color: 'white',
+      fontSize: 30,
+    },
+    header: {
+      // backgroundColor: 'rgb(193, 32, 32)',
+      padding: 20,
+      borderTop: '12px solid rgb(204, 204, 204)'
+    },
+    messageContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      padding: 10,
+      alignItems: 'center'
+    },
+    input: {
+      fontSize: 16,
+      padding: 10,
+      outline: 'none',
+      width: 350,
+      border: 'none',
+      borderBottom: '2px solid rgb(0, 132, 255)'
+    },
+
+  }
+
     return (
       <div className="App">
-        <header className="App-header">
-        <ChatBot 
-          title="My React Bot"
-          botName="BookTripMOBILEHUB"
-          welcomeMessage="Welcome, how can I help you today?"
-          onComplete={this.handleComplete.bind(this)}
-          clearOnComplete={true}
-          />
+        
+        <div class='container'>
+        <header class="headerBlock" style={styles.header}>
+          <p class="header">F.R.I.D.A.Y</p>
         </header>
+        {/* <img src={ironMan} resizeMode='cover' alt="IronMan" style={styles.messageContainer.backgroundImage}/> */}
+          <div class='feed' style={styles.messageContainer}>
+          <h2>{this.state.finalMessage}</h2>
+          <ChatFeed
+            class="chat"  
+            messages={this.state.messages}
+            hasInputField={false}
+            bubbleStyles={styles.bubbleStyles}
+          />
+          <input 
+            onKeyPress={this.handleKeyPress}
+            onChange={this.onChange.bind(this)}
+            style={styles.input}
+            value={this.state.input}
+          />
+          </div>
+        </div>
       </div>
     );
+
+    
   }
+
 }
 
 export default App;
